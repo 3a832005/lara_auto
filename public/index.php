3f1fb5ada67a1d93e51503a0f318cf8916771074
Li Yen Tzu <3a832005@gm.student.ncut.edu.tw>
2022-10-23 02:10:06 +0800

    新增monolog測試程式碼

--- public/index.php
+++ public/index.php
@@ -1,13 +1,30 @@
 <?php
 require '../vendor/autoload.php';
+
 // 類別先import進來
 use Demo\Hello\Lara;
 use Demo\Hello\Someone;
 $lara= new Lara();
 $vincent= new Someone('Vincent');
+
 // 類別未先import進來
 $mary= new \Demo\Hello\Someone('Mary');
 $john= new Demo\Hello\Someone('John');
 $hello= new Demo\HelloWorld();
+
 use Demo\HelloWorld as World; // 類別另取別名
+
 $world = new World();
+
+
+use Monolog\Level;
+use Monolog\Logger;
+use Monolog\Handler\StreamHandler;
+
+// create a log channel
+$log = new Logger('name');
+$log->pushHandler(new StreamHandler('../log/your.log', Level::Warning));
+
+// add records to the log
+$log->warning('Foo');
+$log->error('Bar');
